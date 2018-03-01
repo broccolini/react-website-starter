70dcface6059dfb71846f4e12aac927252aa5bcf
Diana Mounter <broccolini@github.com>
2018-03-01 11:35:14 -0500

    add a span component

---
+++ src/Span.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import {
+  space,
+  width,
+  maxWidth,
+  flex,
+  color
+} from 'styled-system'
+
+const Span = styled.span`
+  ${space}
+  ${width}
+  ${maxWidth}
+  ${flex}
+  ${color}
+`
+
+export default Span
