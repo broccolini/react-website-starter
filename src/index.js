6c88facfdf68f4569e54d395142e2ab6ab00fe97
Brent Jackson <jxnblk@gmail.com>
2018-07-16 18:57:41 -0400

    Remove Page import

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import Page from './Page'
 import theme from './theme'
 import Heading from './Heading'
 import Box from './Box'
 
 const Index = props => (
-  <Page>
-    <Box p={3} mx='auto' width={[1, 1, 2/3,]}>
-      <Heading>
-        Hello world
-      </Heading>
-    </Box>
-  </Page>
+  <Box p={3} mx='auto' width={[1, 1, 2/3,]}>
+    <Heading>
+      Hello world
+    </Heading>
+  </Box>
 )
 
 export default Index
